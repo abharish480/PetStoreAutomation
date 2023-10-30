1e88d49e975fda9062e06540e012efad9a9d7c00
abharish480 <abharish480@gmail.com>
2023-10-30 19:50:17 +0530

    RestAssured Framework with TestNG

---
+++ src/test/java/api/test/UserTests.java
@@ -0,0 +1,88 @@
+package api.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndpoints;
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class UserTests {
+	Faker faker;
+	User userPayload;
+	public Logger logger;
+	
+	@BeforeClass
+	public void setUpData() {
+		faker = new Faker();
+		userPayload= new User();
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5,10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+	
+		logger= LogManager.getLogger(this.getClass());
+	
+	}
+	
+	@Test(priority = 1)
+	public void testPostUser() {
+		
+		logger.info("********* Creating User*************");
+		Response response= UserEndpoints.createUser(userPayload);
+		//System.out.println(userPayload.getUsername());
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("********* User is created *************");
+		
+	}
+	
+	@Test(priority = 2)
+	public void testGetUserByName() {
+		logger.info("********* Retrieving User*************");
+		Response response=UserEndpoints.getUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+	
+	@Test(priority = 3)
+	public void testUpdateUserByName() {
+		
+		logger.info("********* Updating User*************");
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		
+		Response response= UserEndpoints.updateUser(this.userPayload.getUsername(),userPayload);
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		Response responseAfterUpdate=UserEndpoints.getUser(this.userPayload.getUsername());
+		responseAfterUpdate.then().log().all();
+		Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200);
+		
+		logger.info("********* Updated User*************");
+		//Assert.assertEquals(responseAfterUpdat, null);
+	}
+	
+	@Test(priority = 4)
+	public void testDeleteUserByName() {
+		logger.info("********* Deleting User*************");
+		Response response=UserEndpoints.deleteUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("********* User Deleted*************");
+	}
+	
+}
